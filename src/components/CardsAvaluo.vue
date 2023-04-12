9a59540a31092b4e17bb1dd88bc1044f1788f401
dvelagui <dvelagui@gmail.com>
2023-04-12 15:55:58 -0500

    Hero Banner Responsive Ready!

---
+++ src/components/CardsAvaluo.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="card">
+        <img v-show="cardTitle === 'Empresas'" src='../assets/imgs/img_hero_inicio_empresas.png'
+            class="card-img-top position-relative" :alt="cardTitle" :style="cardTitle === 'Empresas' ? '' : 'height: 0;'">
+        <img v-show="cardTitle === 'Personas'" src='../assets/imgs/img_hero_inicio_personas.png'
+            class="card-img-top position-relative" :alt="cardTitle"
+            :style="cardTitle === 'Personas' ? '' : 'height: 0; visible: none'">
+        <img v-show="cardTitle === 'Avalúo certificado'" src='../assets/imgs/img_hero_inicio_avaluo.png'
+            class="card-img-top position-relative" :alt="cardTitle"
+            :style="cardTitle === 'Avalúo certificado' ? '' : 'height: 0; visible: none'">
+        <img v-show="cardTitle === 'Conexión API'" src='../assets/imgs/img_hero_inicio_API.png'
+            class="card-img-top position-relative" :alt="cardTitle"
+            :style="cardTitle === 'Conexión API' ? '' : 'height: 0; visible: none'">
+        <div class="card-body  position-absolute">
+            <h5 class="card-title">{{ cardTitle }}</h5>
+            <p class="card-text">{{ cardText }}</p>
+            <a :href="cardBtn" class="btn btn-primary">Ver más →</a>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { toRefs, computed } from 'vue';
+
+
+const props = defineProps({
+    cardImg: String,
+    cardTitle: String,
+    cardText: String,
+    cardBtn: String,
+});
+
+const { cardImg, cardTitle, cardText, cardBtn } = toRefs(props);
+
+
+const tpt = ["../assets/imgs/img_hero_inicio_empresas.png", "../assets/imgs/img_hero_inicio_empresas.png", "../assets/imgs/img_hero_inicio_empresas.png"
+]
+
+</script>
+
+<style lang="scss" scoped>
+.card {
+    border: none;
+    border-radius: none !important;
+    padding: 0%;
+    width: 25%;
+}
+
+.card-body {
+    bottom: 0;
+    background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.84) 71.31%);
+}
+
+.card-title {
+    font-weight: 600;
+    font-size: 32px;
+    line-height: 100%;
+    letter-spacing: -0.01em;
+    color: #FFFFFF;
+}
+
+.card-text {
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 20px;
+    letter-spacing: -0.01em;
+    color: #FFFFFF;
+}
+
+.btn {
+    border-radius: 32px;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 18px;
+    text-align: center;
+    letter-spacing: -0.01em;
+    color: #FFFFFF;
+
+}
+
+@media (max-width: 992px) {
+   .card {
+    width: 100%;
+    text-align: center;
+   }
+   .card-body {
+    padding: 60px 32px;
+}
+}
+
+</style>
---
+++ src/components/CarouselAvaluo.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <div id="carouselExampleIndicators" class="carousel slide">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+                    aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                    aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                    aria-label="Slide 3"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3"
+                    aria-label="Slide 4"></button>
+            </div>
+            <div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <CardsAvaluo card-img="../assets/imgs/img_hero_inicio_personas.png" card-title="Empresas"
+                            card-text="Ten herramientas para apoyar mejor tu proceso de ventas." card-btn="/empresas" />
+                    </div>
+
+                    <div class="carousel-item">
+                        <CardsAvaluo card-img="../assets/imgs/img_hero_inicio_personas.png" card-title="Personas"
+                            card-text="¿Quieres vender, comprar o arrendar una propiedad y tienes dudas sobre su valor?"
+                            card-btn="/personas" />
+                    </div>
+
+                    <div class="carousel-item">
+                        <CardsAvaluo card-img="../assets/imgs/img_hero_inicio_personas.png" card-title="Avalúo certificado"
+                            card-text="Solicita un informe técnico firmado por un perito inscrito en el RAA."
+                            card-btn="/avaluo-certificado" />
+                    </div>
+
+                    <div class="carousel-item">
+                        <CardsAvaluo card-img="../assets/imgs/img_hero_inicio_personas.png" card-title="Conexión API"
+                            card-text="Conecta la API a tus desarrollos y obtén todas las funcionalidades."
+                            card-btn="/conexion-api" />
+                    </div>
+
+
+                </div>
+
+            </div>
+        </div>
+        <!--   Hero Banner Cards Sections -->
+    </div>
+</template>
+
+<script setup>
+
+import CardsAvaluo from "./CardsAvaluo.vue";
+
+</script>
+
+<style lang="scss" scoped>
+.carousel-indicators {
+      bottom: 0;
+    }
+.carousel-indicators [data-bs-target] {
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+}
+    
+
+</style>
